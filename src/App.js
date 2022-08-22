fe6a68444e1e2568d8315a835af7289a4f90f0e0
Manikarthik Chandankeri <mc21407@corp.ojas-it.com>
2022-08-22 10:58:58 +0530

    reactaxiosapp

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+
+function App()
+ {
+  return (
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route
+              path="/add-employee/:id"
+              component={CreateEmployeeComponent}
+            ></Route>
+            <Route
+              path="/view-employee/:id"
+              component={ViewEmployeeComponent}
+            ></Route>
+          </Switch>
+        </div>
+        <div style={{textAlign:"center"}}>
+           <FooterComponent />
+           </div>
+      </Router>
+    </div>
+  );
+}
+export default App;
